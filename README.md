2d23c67583946c0153dc92347ae964ea20a73a39
Iansmith's win10 <iansmith@qq.com>
2017-07-07 20:48:47 +0800

    add exp

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # s2-048
-Apache struts struts 2 048, CVE-2017-9791.
+Exp for s2-048, CVE-2017-9791.
+
+usage:
+python3 s2-048.py -h www.xx.com/index.action
+
+
---
+++ s2-048.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python3
+#######################################################################
+#     File Name           :     s2-048.py
+#     Created By          :     Les1ie
+#     Email               :     iansmith@qq.com
+#     Creation Date       :     [2017-07-07 20:37]
+#     Last Modified       :     [2017-07-07 20:48]
+#     Description         :     exp for s2-048 
+#######################################################################
+
+import requests
+import sys
+import time
+
+string = '''
+ ____    ____             ___    _  _      ___  
+/ ___|  |___ \           / _ \  | || |    ( _ ) 
+\___ \    __) |  _____  | | | | | || |_   / _ \ 
+ ___) |  / __/  |_____| | |_| | |__   _| | (_) |
+|____/  |_____|          \___/     |_|    \___/ 
+'''
+print(string)
+
+print("CVE-2017-9791")
+print("Refer:")
+print("https://cwiki.apache.org/confluence/display/WW/S2-048?from=groupmessage")
+
